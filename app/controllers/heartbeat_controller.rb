c5f5cd7c780b7f8071c6db90d94b92096fbb3387
Matthew Eagar <meagar@500px.com>
2015-04-24 15:01:54 -0400

    Add heartbeat

--- app/controllers/heartbeat_controller.rb
+++ app/controllers/heartbeat_controller.rb
@@ -2,7 +2,16 @@
 class HeartbeatController < ApplicationController
 
   def index
+    last_letter = NewsLetter.order(:id).last
+    if Time.now.in_time_zone('EST').hour > 9
+      if last_letter.nil? || Time.now - last_letter.created_at > 23.hours
+        @news_letter = NewsLetter.new
+        @news_letter.send!
+        @news_letter.save!
+      end
+    end
 
+    render inline: "Feelin' fine"
   end
 
 end
